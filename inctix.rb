508478326f06260a80628bee4ac370c87043f444
Eric Dahl <eric@skeyelab.com>
2015-10-09 13:41:28 -0500

    proper cycling

--- inctix.rb
+++ inctix.rb
@@ -12,149 +12,153 @@ begin
   puts "start"
   desks = db.query("select * from desks where last_timestamp <= #{Time.now.to_i-300} and wait_till < #{Time.now.to_i} order by last_timestamp desc;")
   #desk = desks.first
-  desks.each do |desk|
-    domain = desk["domain"]
-    client = ZendeskAPI::Client.new do |config|
+  if desks.count > 0
+    desks.each do |desk|
+      domain = desk["domain"]
+      client = ZendeskAPI::Client.new do |config|
 
-      config.url = "https://#{domain}.zendesk.com/api/v2" # e.g. https://mydesk.zendesk.com/api/v2
-      config.username = desk["user"]
-      config.token = desk["token"]
+        config.url = "https://#{domain}.zendesk.com/api/v2" # e.g. https://mydesk.zendesk.com/api/v2
+        config.username = desk["user"]
+        config.token = desk["token"]
 
-      config.retry = false
+        config.retry = false
 
-      # require 'logger'
-      # config.logger = Logger.new(STDOUT)
+        # require 'logger'
+        # config.logger = Logger.new(STDOUT)
 
-    end
-
-
-    client.insert_callback do |env|
-      if env[:status] == 429
-
-        db.query("UPDATE `desks` SET `wait_till` = '#{(env[:response_headers][:retry_after] || 10).to_i + Time.now.to_i}' WHERE `domain` = '#{domain}';")
-        # seconds_left = (env[:response_headers][:retry_after] || 10).to_i
-        # @logger.warn "You have been rate limited. Retrying in #{seconds_left} seconds..." if @logger
-
-        # seconds_left.times do |i|
-        #   sleep 1
-        #   time_left = seconds_left - i
-        #   @logger.warn "#{time_left}..." if time_left > 0 && time_left % 5 == 0 && @logger
-        # end
       end
-    end
 
-    tables = db.query("SHOW TABLES FROM zdtix",:as => :array);
-    tbls =[]
 
-    tables.each do |table|
-      tbls << table[0]
-    end
+      client.insert_callback do |env|
+        if env[:status] == 429
 
-    if !tbls.include? domain
-      db.query("CREATE TABLE `#{domain}` (id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT);
+          db.query("UPDATE `desks` SET `wait_till` = '#{(env[:response_headers][:retry_after] || 10).to_i + Time.now.to_i}' WHERE `domain` = '#{domain}';")
+          # seconds_left = (env[:response_headers][:retry_after] || 10).to_i
+          # @logger.warn "You have been rate limited. Retrying in #{seconds_left} seconds..." if @logger
+
+          # seconds_left.times do |i|
+          #   sleep 1
+          #   time_left = seconds_left - i
+          #   @logger.warn "#{time_left}..." if time_left > 0 && time_left % 5 == 0 && @logger
+          # end
+        end
+      end
+
+      tables = db.query("SHOW TABLES FROM zdtix",:as => :array);
+      tbls =[]
+
+      tables.each do |table|
+        tbls << table[0]
+      end
+
+      if !tbls.include? domain
+        db.query("CREATE TABLE `#{domain}` (id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT);
 ")
-    end
+      end
 
-    starttime = desk["last_timestamp"].to_i
-    begin
+      starttime = desk["last_timestamp"].to_i
+      begin
 
-      puts "Calling #{domain} from #{Time.at(starttime)}"
-      tix = client.tickets.incremental_export(starttime);
-      # puts tix.response.status
-      progressbar = ProgressBar.create(:total => 1000,:format => "%a %e %P% Processed: %c from %C")
+        puts "Calling #{domain} from #{Time.at(starttime)}"
+        tix = client.tickets.incremental_export(starttime);
+        # puts tix.response.status
+        progressbar = ProgressBar.create(:total => 1000,:format => "%a %e %P% Processed: %c from %C")
 
-      tix.each do |tic|
-        results = db.query("SHOW COLUMNS FROM #{domain}");
-        cols = []
+        tix.each do |tic|
+          results = db.query("SHOW COLUMNS FROM #{domain}");
+          cols = []
 
-        results.each do |col|
-          #puts col["Field"]
-          cols << col["Field"]
+          results.each do |col|
+            #puts col["Field"]
+            cols << col["Field"]
 
-        end
-        apicols = []
-        neededcols = []
+          end
+          apicols = []
+          neededcols = []
 
 
-        tic.keys.each do |key|
-          apicols << key
-        end
+          tic.keys.each do |key|
+            apicols << key
+          end
 
-        neededcols = apicols - cols
-        if neededcols.count > 0
+          neededcols = apicols - cols
+          if neededcols.count > 0
 
-          querypairs = []
+            querypairs = []
 
-          neededcols.each do |col|
-            if (col.include? "req_external_id") || (col.include? "_name")
-              pair = {:field => col, :type => "varchar(64)"}
-              querypairs << pair
-            elsif (col.include? "minutes") || (col.include? "id")
-              pair = {:field => col, :type => "int(16)"}
-              querypairs << pair
-            elsif (tix.included["field_headers"][col]) && (tix.included["field_headers"][col].include? "[int]")
-              pair = {:field => col, :type => "int(16)"}
-              querypairs << pair
-            elsif (col.include? "generated_timestamp")
-              pair = {:field => col, :type => "int(16)"}
-              querypairs << pair
-            elsif (col.include? "_at") || (col.include? "timestamp")
-              pair = {:field => col, :type => "datetime"}
-              querypairs << pair
-            elsif col.include? "current_tags"
-              pair = {:field => col, :type => "varchar(1024)"}
-              querypairs << pair
-            else
-              pair = {:field => col, :type => "VARCHAR(255)"}
-              querypairs << pair
+            neededcols.each do |col|
+              if (col.include? "req_external_id") || (col.include? "_name")
+                pair = {:field => col, :type => "varchar(64)"}
+                querypairs << pair
+              elsif (col.include? "minutes") || (col.include? "id")
+                pair = {:field => col, :type => "int(16)"}
+                querypairs << pair
+              elsif (tix.included["field_headers"][col]) && (tix.included["field_headers"][col].include? "[int]")
+                pair = {:field => col, :type => "int(16)"}
+                querypairs << pair
+              elsif (col.include? "generated_timestamp")
+                pair = {:field => col, :type => "int(16)"}
+                querypairs << pair
+              elsif (col.include? "_at") || (col.include? "timestamp")
+                pair = {:field => col, :type => "datetime"}
+                querypairs << pair
+              elsif col.include? "current_tags"
+                pair = {:field => col, :type => "varchar(1024)"}
+                querypairs << pair
+              else
+                pair = {:field => col, :type => "VARCHAR(255)"}
+                querypairs << pair
+              end
             end
+
+            query = "ALTER TABLE #{domain} ADD ("
+
+            querypairs.each do |pair|
+              query = query + pair[:field] + " " + pair[:type]+","
+            end
+            query = query.chomp(",")
+
+            query = query + ");"
+
+            progressbar.log "***ADDING COL***"
+            progressbar.log query
+
+            db.query(query)
+
           end
 
-          query = "ALTER TABLE #{domain} ADD ("
 
-          querypairs.each do |pair|
-            query = query + pair[:field] + " " + pair[:type]+","
+
+          querypairs = {}
+          tic.each do |field|
+            querypairs[field[0].to_s] = field[1]
           end
-          query = query.chomp(",")
+          q1 = "REPLACE INTO #{domain} ("
+          q2 = ") VALUES ("
+          querypairs.each do |key, value|
+            q1 = q1 + key.to_s + ", "
+            q2 = q2 + "\"" + db.escape(value.to_s) + "\", "
+          end
 
-          query = query + ");"
+          q1 = q1.chomp(", ")
+          q2 = q2.chomp(", ")
 
-          progressbar.log "***ADDING COL***"
-          progressbar.log query
+          q2 = q2 + ");"
 
-          db.query(query)
-
+          # puts q1+q2
+          db.query(q1+q2)
+          progressbar.increment
         end
-
-
-
-        querypairs = {}
-        tic.each do |field|
-          querypairs[field[0].to_s] = field[1]
+        oldstarttime = starttime
+        if tix.included
+          #  binding.pry
+          db.query("UPDATE `desks` SET `last_timestamp` = '#{tix.included['end_time']}' WHERE `domain` = '#{domain}';")
+          starttime = tix.included['end_time']
         end
-        q1 = "REPLACE INTO #{domain} ("
-        q2 = ") VALUES ("
-        querypairs.each do |key, value|
-          q1 = q1 + key.to_s + ", "
-          q2 = q2 + "\"" + db.escape(value.to_s) + "\", "
-        end
-
-        q1 = q1.chomp(", ")
-        q2 = q2.chomp(", ")
-
-        q2 = q2 + ");"
-
-        # puts q1+q2
-        db.query(q1+q2)
-        progressbar.increment
-      end
-      oldstarttime = starttime
-      if tix.included
-        #  binding.pry
-        db.query("UPDATE `desks` SET `last_timestamp` = '#{tix.included['end_time']}' WHERE `domain` = '#{domain}';")
-        starttime = tix.included['end_time']
-      end
-      progressbar.finish
-    end while ((oldstarttime < starttime) && (oldstarttime < Time.now.to_i))
+        progressbar.finish
+      end while ((oldstarttime < starttime) && (oldstarttime < Time.now.to_i))
+    end
+  else
+    sleep 10
   end
 end while 1
